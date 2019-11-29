8935f91814253699616573b7dd43bc0fef430943
xiaosizhe <xiaosizheme@gmail.com>
2019-11-29 17:19:31 +0800

    delete sql file

--- update_sample.sql
+++
@@ -1,2 +0,0 @@
-delete from sample_attribute where sample_id in (select sample_id from dataset_sample where dataset_id in (select id from dataset where identifier='100XXX'));
-delete from sample where id in (select sample_id from dataset_sample where dataset_id in (select id from dataset where identifier = '100XXX'));
